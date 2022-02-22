05d7257b244dd1f45acd141ed174c8c206698d4d
Umut Dag <umutdag156@gmail.com>
2022-02-22 19:46:24 +0300

    NodeJS-Command-Prompt-Odev1-is-done

---
+++ NodeJS/Command_Prompt_Odev1/script.js
@@ -0,0 +1,9 @@
+// Not needed to be Imported
+const radius = process.argv[2] ? // Getting Command Prompt Third Param 
+	process.argv[2] : // Which is in Second Position in an Arr
+	0; // If It's not Defined, Then Assign to 0(Zero) as radius
+const PI = Math.PI; // Getting PI
+
+const circleArea = (radius) => PI * radius ** 2; // Calculating Circle Area and Getting Result
+
+console.log(circleArea(radius));  // Print it to Console
