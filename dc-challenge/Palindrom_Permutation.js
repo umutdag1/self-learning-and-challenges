0bc438553e9b62f7598f56bd65923bb0aff0238c
Umut Dag <umutdag156@gmail.com>
2022-02-28 19:58:42 +0300

    dc-challenge-are-updated

--- dc-challenge/Palindrom_Permutation.js
+++ dc-challenge/Palindrom_Permutation.js
@@ -1,39 +1,81 @@
-const WORD = "Coo Coo"; // You Can Change This
-
+const WORD = "Tact Coa"; // You Can Change This
 const onlyCheckOne = true; // You Can Change This
-// NOTE : If you set onlyCheckOne as false, you may get results lately
-
-showResult(onlyCheckOne);
+const onlyStatus = true; // You Can Change This
+// NOTE : The fastest way to get result quickly is to set true both onlyCheckOne and onlyStatus
+/****************************************/
+// onlyCheckOne = true && onlyStatus = 
+// true --> Getting Only Count of Result (Fast)
+// onlyCheckOne = true && onlyStatus = 
+// false --> Getting Only For One Record For Both Count and Result Word (Getting Slow as Size Increase)
+// onlyCheckOne = false && onlyStatus = 
+// false --> Getting Both Count and Result Words in Array (Getting Slow as Size Increase)
+/****************************************/
+showResult(onlyCheckOne, onlyStatus);
 
 /* Handling Word Functions */
 
-function showResult(onlyCheckOne) {
-    const palindromPermutations = doOperation(onlyCheckOne);
+function showResult(onlyCheckOne, onlyStatus) {
+    if(!onlyCheckOne) {
+        onlyStatus = false;
+    }
+
+    const palindromPermutations = doOperation(onlyCheckOne, onlyStatus);
 
     if (onlyCheckOne) {
-        const status = palindromPermutations.length === 1 ? 'Yes' : 'No';
-        console.log(`Result of "${WORD}" palindrome permutation : `);
-        console.log(`Status : ${status}, Result : "${palindromPermutations[0] ? palindromPermutations[0] : ''}"`);
+        if (onlyStatus) {
+            const status = palindromPermutations ? 'Yes' : 'No';
+            console.log(`Result of "${WORD}" palindrome permutation : `);
+            console.log(`Status : ${status}`);
+        } else {
+            const status = palindromPermutations.length === 1 ? 'Yes' : 'No';
+            console.log(`Result of "${WORD}" palindrome permutation : `);
+            console.log(`Status : ${status}, Result : "${palindromPermutations[0] ? palindromPermutations[0] : ''}"`);
+        }
     } else {
         console.log(`Result of "${WORD}" palindrome permutations : `);
         console.log(`Count : ${palindromPermutations.length}, Result Array :`, palindromPermutations);
     }
 }
 
-function doOperation(onlyCheckOne) {
-    const charArrOfWord = getCharArray('', WORD); // Don't Touch
+function doOperation(onlyCheckOne, onlyStatus) {
+    const nonCaseSensitiveWord = getStrLowerOrUpperCase(WORD);
+    const charArrOfWord = getCharArray('', nonCaseSensitiveWord); // Don't Touch
     const charSpaceIndexArrOfWord = getIndexOfSearchStrInArray(' ', charArrOfWord); // Don't Touch
-    const nonSpacesCharArrOfWord = getCharArray('', getNonSpacesStr(WORD)); // Don't Touch
+    const nonSpacesCharArrOfWord = getCharArray('', getNonSpacesStr(nonCaseSensitiveWord)); // Don't Touch
 
-    const palindromPermutations = getPalindromPermutationsOfWord(
-        nonSpacesCharArrOfWord,
-        charSpaceIndexArrOfWord,
-        "",
-        [],
-        onlyCheckOne
-    ); // Don't Touch
+    let result = null;
 
-    return palindromPermutations;
+    if (onlyStatus) {
+        result = getPalindromPermutationsOfWordStatus(
+            nonSpacesCharArrOfWord
+        );
+    } else {
+        result = getPalindromPermutationsOfWord(
+            nonSpacesCharArrOfWord,
+            charSpaceIndexArrOfWord,
+            "",
+            [],
+            onlyCheckOne
+        ); // Don't Touch
+    }
+
+    return result;
+}
+
+function getPalindromPermutationsOfWordStatus(charArr) {
+    const charArrLength = charArr.length;
+    const isArrLengthOdd = charArrLength % 2 !== 0;
+
+    const countCharObj = getCountOfChar(charArr);
+    const newCharArrCounts = Object.values(countCharObj);
+
+    if (isArrLengthOdd) {
+        const oddCounts = newCharArrCounts.filter(count => count % 2 !== 0);
+        return oddCounts.length === 1 ? true : false;
+    } else {
+        const oddCounts = newCharArrCounts.filter(count => count % 2 !== 0);
+        return oddCounts.length === 0 ? true : false;
+    }
 }
 
 function getPalindromPermutationsOfWord(charArr, charSpaceIndexArr, str, palindromPermutations, onlyCheckOne) {
@@ -65,6 +107,17 @@ function getPalindromPermutationsOfWord(charArr, charSpaceIndexArr, str, palindr
     return palindromPermutations;
 }
 
+function getCountOfChar(charArr) {
+    return charArr.reduce((newObj, char) => {
+        if (!isNaN(newObj[char])) {
+            newObj[char]++;
+        } else {
+            newObj[char] = 1;
+        }
+        return newObj;
+    }, {});
+}
+
 function checkPalindrom(charArr) {
     let isPalindrom = true;
 
@@ -78,6 +131,14 @@ function checkPalindrom(charArr) {
     }
 
     return isPalindrom;
+}
+
+function getStrLowerOrUpperCase(str, caseType = 'lower') {
+    if (caseType === 'upper') {
+        return str.toUpperCase();
+    } else {
+        return str.toLowerCase();
+    }
 }
 
 function getCharArray(separator, str) {
