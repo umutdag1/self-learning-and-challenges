00a43a1e5b3e0bc5d578902b7a259851f096dad1
Umut Dag <umutdag156@gmail.com>
2022-03-03 23:27:55 +0300

    Coderbyte-Sudoku-Filename-is-changed-and-dynamic-feature-is-added

--- coderbyte-hard-js/Sudoku_Quadrant_Checker/Solution_Sudoku_Quadrant_Checker.js
+++ coderbyte-hard-js/Sudoku_Quadrant_Checker/Solution_Sudoku_Quadrant_Checker.js
@@ -1,18 +1,42 @@
-const rowLength = 9;
-const columnLength = 9;
-const quadrantPerRowLength = 3;
+// Test Case 1
+// const length = 4;
+// const quadrantRules = [
+//     [2, 2], [2, 4],
+//     [4, 2], [4, 4],
+// ];
+// const sudokuQuadrants = [
+//     "(1,1,x,x)",
+//     "(1,x,1,x)",
+//     "(x,x,x,x)",
+//     "(x,2,2,x)",
+// ];
 
-/*const sudokuQuadrants = [
-    "(1,2,3,4,5,6,7,8,9)",
-    "(x,x,x,x,x,x,x,x,x)",
-    "(6,x,5,x,3,x,x,4,x)",
-    "(2,x,1,1,x,x,x,x,x)",
-    "(x,x,x,x,x,x,x,x,x)",
-    "(x,x,x,x,x,x,x,x,x)",
-    "(x,x,x,x,x,x,x,x,x)",
-    "(x,x,x,x,x,x,x,x,x)",
-    "(x,x,x,x,x,x,x,x,9)"
-];*/
+// Test Case 2
+// const length = 9;
+// const quadrantRules = [
+//     [3, 3], [3, 6], [3, 9],
+//     [6, 3], [6, 6], [6, 9],
+//     [9, 3], [9, 6], [9, 9]
+// ];
+// const sudokuQuadrants = [
+//     "(1,2,3,4,5,6,7,8,9)",
+//     "(x,x,x,x,x,x,x,x,x)",
+//     "(6,x,5,x,3,x,x,4,x)",
+//     "(2,x,1,1,x,x,x,x,x)",
+//     "(x,x,x,x,x,x,x,x,x)",
+//     "(x,x,x,x,x,x,x,x,x)",
+//     "(x,x,x,x,x,x,x,x,x)",
+//     "(x,x,x,x,x,x,x,x,x)",
+//     "(x,x,x,x,x,x,x,x,9)"
+// ];
+
+// Test Case 3
+const length = 9;
+const quadrantRules = [
+    [3, 3], [3, 6], [3, 9],
+    [6, 3], [6, 6], [6, 9],
+    [9, 3], [9, 6], [9, 9]
+];
 const sudokuQuadrants = [
     "(1,2,3,4,5,6,7,8,1)",
     "(x,x,x,x,x,x,x,x,x)",
@@ -26,19 +50,19 @@ const sudokuQuadrants = [
 ];
 
 const sudokuQuadrantValues = getSudokuQuadrantValues(sudokuQuadrants);
-const reverseSudokuQuadrantValues = getReverseSudokuQuadrantValues(sudokuQuadrants, rowLength, columnLength);
-const partialSudokuQuadrantValues = getPartialSudokuQuadrantValues(sudokuQuadrants, rowLength, columnLength, quadrantPerRowLength);
+const reverseSudokuQuadrantValues = getReverseSudokuQuadrantValues(sudokuQuadrants, length, length);
+const partialSudokuQuadrantValues = getPartialSudokuQuadrantValues(sudokuQuadrants, quadrantRules, length, length);
 
 let result = [];
 
-result.push(checkQuadrant(sudokuQuadrantValues, rowLength, columnLength, 'horizontal'));
-result.push(checkQuadrant(reverseSudokuQuadrantValues, columnLength, rowLength, 'vertical'));
-result.push(checkQuadrant(partialSudokuQuadrantValues, columnLength, rowLength, 'all'));
+result.push(checkQuadrant(sudokuQuadrantValues, quadrantRules, length, length, 'horizontal'));
+result.push(checkQuadrant(reverseSudokuQuadrantValues, quadrantRules, length, length, 'vertical'));
+result.push(checkQuadrant(partialSudokuQuadrantValues, quadrantRules, length, length, 'all'));
 
 console.log(sudokuQuadrants);
-console.log("Error Quadrants :",[...new Set(result.flat())].sort());
+console.log("Error Quadrants :", [...new Set(result.flat())].sort());
 
-function checkQuadrant(numericSudokuQuadrants, rowLength, columnLength, type) {
+function checkQuadrant(numericSudokuQuadrants, rules, rowLength, columnLength, type) {
     let quadrants = [];
 
     for (let i = 0; i < rowLength; i++) {
@@ -51,11 +75,11 @@ function checkQuadrant(numericSudokuQuadrants, rowLength, columnLength, type) {
 
             if (nums.split(checkNum).length - 1 > 1) {
                 if (type === 'horizontal') {
-                    quadrants.push(getQuadrantLocation(i, j));
+                    quadrants.push(getQuadrantLocation(i, j, rules));
                 } else if (type === 'vertical') {
-                    quadrants.push(getQuadrantLocation(j, i));
+                    quadrants.push(getQuadrantLocation(j, i, rules));
                 } else if (type === 'all') {
-                    quadrants.push(getQuadrantLocation(i, j));
+                    quadrants.push(i + 1);
                 }
             }
         }
@@ -86,13 +110,15 @@ function getReverseSudokuQuadrantValues(sudokuQuadrants, rowLength, columnLength
     return reverseSudokuQuadrants;
 }
 
-function getPartialSudokuQuadrantValues(sudokuQuadrants, rowLength, columnLength, quadrantLength) {
+function getPartialSudokuQuadrantValues(sudokuQuadrants, quadrantRules, rowLength, columnLength) {
     const sudokuQuadrantValues = getSudokuQuadrantValues(sudokuQuadrants);
     const partialSudokuQuadrants = [];
 
     for (let i = 0; i < rowLength; i++) {
         for (let j = 0; j < columnLength; j++) {
-            const quadrantLocation = getQuadrantLocation(i, j) - 1;
+
+            const quadrantLocation = getQuadrantLocation(i, j, quadrantRules) - 1;
+
             if (!partialSudokuQuadrants[quadrantLocation]) {
                 partialSudokuQuadrants.push(sudokuQuadrantValues[i][j]);
             } else {
@@ -104,18 +130,10 @@ function getPartialSudokuQuadrantValues(sudokuQuadrants, rowLength, columnLength
     return partialSudokuQuadrants;
 }
 
-function getQuadrantLocation(row, column) {
-    if (row < 3) {
-        if (column < 3) return 1;
-        else if (column < 6) return 2;
-        else return 3;
-    } else if (row < 6) {
-        if (column < 3) return 4;
-        else if (column < 6) return 5;
-        else return 6;
-    } else {
-        if (column < 3) return 7;
-        else if (column < 6) return 8;
-        else return 9;
+function getQuadrantLocation(row, column, rules) {
+    for (let i = 0; i < rules.length; i++) {
+        if (row < rules[i][0] && column < rules[i][1]) {
+            return i + 1;
+        }
     }
 }
